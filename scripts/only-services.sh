5da329d1da8f094c7c35d5aefb60056ce5fcedcb
Benehiko <alanoterblanche@gmail.com>
2021-02-19 12:37:00 +0200

    fix(initialise): fix client creation

---
+++ scripts/only-services.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker-compose -f .docker/hydra-postgresql.yml \
+ -f .docker/hydra-mysql.yml \
+ -f .docker/jaeger.yml \
+ up --build --remove-orphans --force-recreate
--- tester.py
+++ tester.py
@@ -8,18 +8,21 @@ import admin_client as ac
 def initialise(admin_client, public_client, clients=1000):
     oauth_clients = []
 
-    async def _call_gen():
-        return ac.gen_client(admin_client=admin_client)
+    for i in range(0, clients):
+        oauth_clients.append(ac.gen_client(admin_client))
 
-    async def _inner():
-        tasks = []
-        for i in clients:
-            tasks.append(asyncio.ensure_future(_call_gen))
-
-        oauth_clients = await asyncio.gather(*tasks)
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(_inner())
+    # async def _call_gen():
+    #     return ac.gen_client(admin_client=admin_client)
+    #
+    # async def _inner():
+    #     tasks = []
+    #     for i in clients:
+    #         tasks.append(asyncio.create_task(_call_gen()))
+    #
+    #     oauth_clients = await asyncio.gather(*tasks)
+    #
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(_inner())
     return oauth_clients
 
 
@@ -34,23 +37,24 @@ def tester(args):
         }
 
     if args.run_for:
-        config.run_for = args.run_for
+        config["run_for"] = args.run_for
 
     if args.clients:
-        config.clients = args.clients
+        config["clients"] = args.clients
 
     if args.failure_rate:
-        config.failure_rate = args.failure_rate
+        config["failure_rate"] = args.failure_rate
 
     if args.host:
-        print('host')
-        config.host = args.host
+        config["host"] = args.host
 
     if args.admin_port:
-        config.admin_port = args.admin_port
+        config["admin_port"] = args.admin_port
 
     if args.public_port:
-        config.public_port = args.public_port
+        config["public_port"] = args.public_port
+
+    print('Running with configs:', config)
 
     admin_url = f'http://{config["host"]}:{config["admin_port"]}'
     public_url = f'http://{config["host"]}:{config["public_port"]}'
@@ -63,11 +67,22 @@ def tester(args):
     public_config.host = public_url
     public_client = hydra.ApiClient(configuration=public_config)
 
-    wait_time = 1
+    wait_time = 2
+    max_time = 100
     while True:
         try:
-            initialise(admin_client, public_client)
-        except:
+            oauth_clients = initialise(admin_client, public_client, clients=config["clients"])
+            print(f'no. oauth clients created successfully: {len(oauth_clients)}')
+            print(f'clients: ', oauth_clients)
+
+            break
+        except Exception as e:
+            print(e)
             pass
         wait_time *= 2
+        if wait_time > max_time:
+            print(
+                f'Ending tester due to initialisation failure. Wait time has exceeded max_time: '
+                f'({wait_time} > {max_time})')
+            break
         time.sleep(wait_time)
